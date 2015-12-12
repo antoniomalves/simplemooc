8051cdc15d1d4be219f4716a0700d7f32dd3efd9
Antonio Marcos Alves <antonio.malves.lima@gmail.com>
2015-12-12 09:13:35 -0200

    Criado a view no core e testado o conteudo com um HelloWorld

--- simplemooc/urls.py
+++ simplemooc/urls.py
@@ -4,9 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'simplemooc.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'simplemooc.core.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
